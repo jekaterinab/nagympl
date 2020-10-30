9b04555704c0e6ce381de7e1e65bfbd00b9dc155
keytee <realkeytee@gmail.com>
2020-10-30 17:24:37 +0100

    Odstraneny testovaci soubory, doplnene funkcni nebo rozpracovane

--- test pydrive.py
+++ test pydrive.py
@@ -6,6 +6,7 @@ gauth.LocalWebserverAuth()
 GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = "d:/nagympl/client_secrets.json" #zde zadej cestu k souboru client_secrets.json. měl by být ve stejné složce s tímto scriptem
 drive = GoogleDrive(gauth)
 #vypíše to seznam souborů v hlavním katalogu tvého accountu na google drive
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+#file_list = drive.ListFile({'q': "'dokumenty' in parents and trashed=false"}).GetList()
+file_list = drive.ListFile({'q': "'dokumenty' in 'dokumenty'"}).GetList()
 for file1 in file_list:
   print('title: %s, id: %s' % (file1['title'], file1['id']))
